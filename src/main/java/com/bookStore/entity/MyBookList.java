f44ce698d9febe578a5f5c8623c1241cd225d97f
RadBile2022 <wahradar511@gmail.com>
2023-11-22 14:38:18 +0700

    fix app

---
+++ src/main/java/com/bookStore/entity/MyBookList.java
@@ -0,0 +1,56 @@
+package com.bookStore.entity;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+//import javax.persistence.Column;
+//import javax.persistence.Entity;
+//import javax.persistence.Table;
+
+//@Entity
+//@Table(name="MyBooks")
+@Document(collection = "MyBooks")
+public class MyBookList {
+	
+//	@Id
+	@Id
+	private int id;
+	private String name;
+	private String author;
+	private String price;
+	public MyBookList() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public MyBookList(int id, String name, String author, String price) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.author = author;
+		this.price = price;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+}
---
+++ src/main/java/com/bookStore/service/BookService.java
@@ -0,0 +1,34 @@
+package com.bookStore.service;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bookStore.entity.Book;
+import com.bookStore.repository.BookRepository;
+
+@Service
+public class BookService {
+	
+	@Autowired
+	private BookRepository bRepo;
+	
+	public void save(Book b) {
+		b.setId(Integer.parseInt(String.valueOf(UUID.randomUUID().hashCode())));
+
+		bRepo.save(b);
+	}
+	
+	public List<Book> getAllBook(){
+		return bRepo.findAll();
+	}
+	
+	public Book getBookById(int id) {
+		return bRepo.findById(id).get();
+	}
+	public void deleteById(int id) {
+		bRepo.deleteById(id);
+	}
+}
