d8c82a422ece330d1a4b91f9f2e9cfe05dc05657
Radar <102400087+RadBile2022@users.noreply.github.com>
2023-11-24 13:12:18 +0700

    Update BookStoreApplication.java

--- src/main/java/com/bookStore/BookStoreApplication.java
+++ src/main/java/com/bookStore/BookStoreApplication.java
@@ -2,12 +2,18 @@ package com.bookStore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 
 @SpringBootApplication
-public class BookStoreApplication {
+public class BookStoreApplication implements WebServerFactoryCustomizer<ConfigurableWebServerFactory>{
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookStoreApplication.class, args);
+	}
+	@Override
+	public void customize(ConfigurableWebServerFactory factory) {
+		factory.setPort(7070);
 	}
 
 }
